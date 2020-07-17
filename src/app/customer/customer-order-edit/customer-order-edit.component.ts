4f8c83f53560d474bbd2ec8ae4f7af28161cac63
whoseRon <whose69@gmail.com>
2020-07-18 02:27:55 +0300

    Install project + add customerOrderEdit component

---
+++ src/app/customer/customer-order-edit/customer-order-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cm-customer-order-edit',
+  templateUrl: './customer-order-edit.component.html',
+  styleUrls: ['./customer-order-edit.component.css']
+})
+export class CustomerOrderEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerOrderEditComponent } from './customer-order-edit/customer-order-edit.component';
 
 @NgModule({
   imports: [CustomerRoutingModule, SharedModule],
-  declarations: [CustomerRoutingModule.components]
+  declarations: [CustomerRoutingModule.components, CustomerOrderEditComponent]
 })
 export class CustomerModule { }
