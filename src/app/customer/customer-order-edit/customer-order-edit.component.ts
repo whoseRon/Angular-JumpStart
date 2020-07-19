d44bed621da7c3b77e985a80d2e00de4921bc1a5
whoseRon <whose69@gmail.com>
2020-07-19 04:13:20 +0300

    Make add order works

--- src/app/customer/customer-order-edit/customer-order-edit.component.ts
+++ src/app/customer/customer-order-edit/customer-order-edit.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
-import { IOrder } from 'src/app/shared/interfaces';
+import { IOrder, ICustomer } from 'src/app/shared/interfaces';
 import { DataService } from 'src/app/core/services/data.service';
-import { GrowlerService } from 'src/app/core/growler/growler.service';
+import { GrowlerService, GrowlerMessageType } from 'src/app/core/growler/growler.service';
 import { ModalService } from 'src/app/core/modal/modal.service';
 import { LoggerService } from 'src/app/core/services/logger.service';
 
@@ -20,6 +20,8 @@ export class CustomerOrderEditComponent implements OnInit {
     itemCost: 0
   };
   userId: number;
+  customer: ICustomer;
+  errorMessage: string;
 
   @ViewChild('orderForm', { static: true }) orderForm: NgForm;
 
@@ -31,13 +33,36 @@ export class CustomerOrderEditComponent implements OnInit {
     private logger: LoggerService) { }
 
   ngOnInit(): void {
-    this.userId = this.route.parent.params['id'];
+    this.userId = +this.route.parent.snapshot.params['id'];
+    this.getCustomer(this.userId);
+  }
+
+  getCustomer(id: number) {
+    this.dataService.getCustomer(id).subscribe((customer: ICustomer) => {
+      this.customer = customer;
+    });
+  }
+
+  submit() {
+    this.customer.orders.push(this.order);
+      this.dataService.updateCustomer(this.customer)
+        .subscribe((status: boolean) => {
+          if (status) {
+            this.growler.growl('Operation performed successfully.', GrowlerMessageType.Success);
+            this.router.navigate([`/customers/${this.userId}/orders`]);
+          } else {
+            const msg = 'Unable to add order to user';
+            this.growler.growl(msg, GrowlerMessageType.Danger);
+            this.errorMessage = msg;
+          }
+        },
+          (err: any) => this.logger.log(err));
   }
 
   cancel(event: Event) {
     event.preventDefault();
     // Route guard will take care of showing modal dialog service if data is dirty
-    this.router.navigate(['/customers']);
+    this.router.navigate([`/customers/${this.userId}/orders`]);
   }
 
 }
